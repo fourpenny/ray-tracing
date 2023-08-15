23a4eec33a6d1e062ed1135e773adf2a17fcab1f
Christian Clark <christianclark687@gmail.com>
2023-08-15 14:24:07 -0400

    Completed basic vec3 struct

--- src/vec.rs
+++ src/vec.rs
@@ -1,3 +1,4 @@
+#[derive(Clone,Copy)]
 struct Vec3 {
     e: [f64; 3],
 }
@@ -25,18 +26,17 @@ impl Vec3 {
     }
     
     // Vector operations
-    fn double_dot(&self, other: Vec3) -> Vec3 {
+    fn length_squared(&self) -> f64 {
+        self.e[0] * self.e[0] + self.e[1] * self.e[1] + self.e[2] * self.e[2]
     }
 
-    fn cross(&self, other: Vec3) -> Vec3 {
-    }
-
-    fn unit_vector(&self) -> Vec3 {
+    fn length(&self) -> f64 {
+        self.length_squared().sqrt()
     }
 }
 
 // Basic Operations
-impl std::ops::Add for Vec3 {
+impl std::ops::Add<Self> for Vec3 {
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
@@ -50,5 +50,153 @@ impl std::ops::Add for Vec3 {
     }
 }
 
-// Default access operators
+impl std::ops::Sub<Self> for Vec3 {
+    type Output = Self;
 
+    fn sub(self, other: Self) -> Self {
+        Vec3 {
+            e: [
+                self.e[0] - other.e[0],
+                self.e[1] - other.e[1],
+                self.e[2] - other.e[2],
+            ],
+        }
+    }
+}
+
+impl std::ops::Mul<Self> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        Vec3 {
+            e: [
+                self.e[0] * other.e[0],
+                self.e[1] * other.e[1],
+                self.e[2] * other.e[2],
+            ],
+        }
+    }
+}
+
+// Generic implementation of division by a scalar
+impl<T> std::ops::Div<T> for Vec3
+where
+    T: Into<f64> + Copy,
+{
+    type Output = Vec3;
+
+    fn div(self, scalar: T) -> Vec3 {
+        let scalar_f64: f64 = scalar.into();
+
+        Vec3 {
+            e: [
+                self.e[0] / scalar_f64,
+                self.e[1] / scalar_f64,
+                self.e[2] / scalar_f64,
+            ],
+        }
+    }
+}
+
+// Generic implementation of multiplication by a scalar
+impl<T> std::ops::Mul<T> for Vec3
+where
+    T: Into<f64> + Copy,
+{
+    type Output = Vec3;
+
+    fn mul(self, scalar: T) -> Vec3 {
+        let scalar_f64: f64 = scalar.into();
+
+        Vec3 {
+            e: [
+                self.e[0] * scalar_f64,
+                self.e[1] * scalar_f64,
+                self.e[2] * scalar_f64,
+            ],
+        }
+    }
+}
+
+// Assignment operators
+impl<T> std::ops::MulAssign<T> for Vec3
+where
+    T: Into<f64> + Copy,
+{
+    fn mul_assign(&mut self, scalar: T) {
+        let scalar_f64: f64 = scalar.into();
+        self.e[0] *= scalar_f64;
+        self.e[1] *= scalar_f64;
+        self.e[2] *= scalar_f64;
+    }
+}
+
+impl<T> std::ops::AddAssign<T> for Vec3
+where
+    T: Into<f64> + Copy,
+{
+    fn add_assign(&mut self, scalar: T) {
+        let scalar_f64: f64 = scalar.into();
+        self.e[0] += scalar_f64;
+        self.e[1] += scalar_f64;
+        self.e[2] += scalar_f64;
+    }
+}
+
+impl<T> std::ops::SubAssign<T> for Vec3
+where
+    T: Into<f64> + Copy,
+{
+    fn sub_assign(&mut self, scalar: T) {
+        let scalar_f64: f64 = scalar.into();
+        self.e[0] -= scalar_f64;
+        self.e[1] -= scalar_f64;
+        self.e[2] -= scalar_f64;
+    }
+}
+
+impl<T> std::ops::DivAssign<T> for Vec3
+where
+    T: Into<f64> + Copy,
+{
+    fn div_assign(&mut self, scalar: T) {
+        let scalar_f64: f64 = scalar.into();
+        self.e[0] /= scalar_f64;
+        self.e[1] /= scalar_f64;
+        self.e[2] /= scalar_f64;
+    }
+}
+
+// Default access operators
+impl std::ops::Index<usize> for Vec3 {
+    type Output = f64;
+
+    fn index(&self, i: usize) -> &f64 {
+        &self.e[i]
+    }
+}
+
+impl std::ops::IndexMut<usize> for Vec3 {
+    fn index_mut(&mut self, i: usize) -> &mut f64 {
+        &mut self.e[i]
+    }
+}
+
+// Operations between vectors
+fn cross(u: Vec3, v: Vec3) -> Vec3 {
+    Vec3{
+        e: [
+            u.e[1] * v.e[2] - u.e[2] * v.e[1],
+            u.e[2] * v.e[0] - u.e[0] * v.e[2],
+            u.e[0] * v.e[1] - u.e[1] * v.e[0]
+        ],
+    }
+}
+
+fn unit_vector(v: Vec3) -> Vec3 {
+    v / v.length() 
+}
+
+fn double_dot(u: &Vec3, v: &Vec3) -> f64 {
+    u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2] 
+}
